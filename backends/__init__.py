f65c1c38f406a92c806d1ae84841fc2c0cd3e199
Erick Licona <92254077+YaBraG@users.noreply.github.com>
2026-06-18 11:09:56 -0400

    Add backend package skeleton

---
+++ backends/__init__.py
@@ -0,0 +1,9 @@
+"""Movement backend package.
+
+TODO:
+- Keep backend imports lazy.
+- Do not import Quanser libraries here.
+- Use QBotMover as the public entry point instead of backend classes directly.
+"""
+
+__all__: list[str] = []
