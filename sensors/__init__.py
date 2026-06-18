b9134eb7af52abab3f1b0fc648983b07851af44d
Erick Licona <92254077+YaBraG@users.noreply.github.com>
2026-06-18 11:13:29 -0400

    Add sensor package skeleton

---
+++ sensors/__init__.py
@@ -0,0 +1,9 @@
+"""Sensor wrapper package.
+
+TODO:
+- Keep each sensor implementation isolated.
+- Do not import Quanser or RealSense libraries from this file.
+- Expose stable sensor interfaces after wrappers are implemented.
+"""
+
+__all__: list[str] = []
