52c10c32c96a99b8d5d2de03c556a1e3a71f5842
Erick Licona <92254077+YaBraG@users.noreply.github.com>
2026-06-18 11:03:14 -0400

    Add qbot package skeleton

---
+++ qbot/__init__.py
@@ -0,0 +1,11 @@
+"""Core QBot Platform package.
+
+This package should expose the clean project API, not Quanser's raw APIs.
+
+TODO:
+- Export QBotMover when qbot.mover is implemented.
+- Export QBotState and QBotCommand when the state model is stable.
+- Keep imports lightweight so importing qbot does not require Quanser libraries.
+"""
+
+__all__ = []
