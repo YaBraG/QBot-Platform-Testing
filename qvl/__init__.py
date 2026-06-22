0682c8dca9afc9a0e85dd9b804874689e2d58432
Erick Licona <92254077+YaBraG@users.noreply.github.com>
2026-06-22 14:15:16 -0400

    Add QVL compatibility package

---
+++ qvl/__init__.py
@@ -0,0 +1,5 @@
+"""Compatibility package that forwards to virtual_dependencies.quanser_qvl."""
+
+from virtual_dependencies.quanser_qvl import CommModularContainer, QLabsQBotPlatform, QuanserInteractiveLabs
+
+__all__ = ["CommModularContainer", "QLabsQBotPlatform", "QuanserInteractiveLabs"]
