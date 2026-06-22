4b1cf9cccbe13b363f1fa14d20c7f595351ce3f4
Erick Licona <92254077+YaBraG@users.noreply.github.com>
2026-06-22 14:15:32 -0400

    Add qvl qlabs forwarding module

---
+++ qvl/qlabs.py
@@ -0,0 +1,5 @@
+"""Forwarding module for local virtual dependency package."""
+
+from virtual_dependencies.quanser_qvl.qlabs import CommModularContainer, QuanserInteractiveLabs
+
+__all__ = ["CommModularContainer", "QuanserInteractiveLabs"]
