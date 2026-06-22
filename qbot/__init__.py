244e48d7dab93e577a0ae7e0a36e03fad1b24bcf
Erick Licona <92254077+YaBraG@users.noreply.github.com>
2026-06-22 12:11:45 -0400

    Export stable QBot API objects

--- qbot/__init__.py
+++ qbot/__init__.py
@@ -1,12 +1,16 @@
-"""Core QBot Platform package.
+"""Core QBot Platform package."""
 
-This package exposes the clean project API. It should not expose raw Quanser
-classes directly.
+from qbot.environment import MODE_MOCK, MODE_PHYSICAL, MODE_VIRTUAL
+from qbot.mover import QBotMover
+from qbot.state import CameraFrame, LidarScan, QBotCommand, QBotState
 
-TODO:
-- Export QBotMover after the mover API is stable.
-- Export QBotState and QBotCommand after the data model is stable.
-- Keep this file lightweight so importing qbot does not require Quanser.
-"""
-
-__all__: list[str] = []
+__all__ = [
+    "CameraFrame",
+    "LidarScan",
+    "MODE_MOCK",
+    "MODE_PHYSICAL",
+    "MODE_VIRTUAL",
+    "QBotCommand",
+    "QBotMover",
+    "QBotState",
+]
